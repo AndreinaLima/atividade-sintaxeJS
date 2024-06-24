5d679185229ec87ee10dc2c7f2fce4dc31de6d23
Andreina Lima <andreinaadelima14@gmail.com>
2024-06-24 12:17:51 -0300

    Adicionado arquivos conversao e variaveis js

---
+++ conversao.js
@@ -0,0 +1,8 @@
+let F = 50;
+let resultadoEmC = (F - 32) / 1.8;
+
+console.log(resultadoEmC.toFixed(2));
+
+
+
+
---
+++ variaveis.js
@@ -0,0 +1,9 @@
+let nota1 = 8 * 3;
+let nota2 = 7.5 * 2;
+let nota3 = 6.5 * 1;
+let nota4 = 9 * 4;
+let nota5 = 5 * 5;
+
+mediaPonderada = (nota1 + nota2 + nota3 + nota4 + nota5) / 15
+
+console.log("Média ponderada: ", mediaPonderada)
